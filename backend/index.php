989eaedd1961badab19cb51dceb9afc87d9aa534
heve-dev <hevellin.jsantos@hotmail.com>
2025-09-26 10:41:55 -0300

    cm

--- backend/index.php
+++ backend/index.php
@@ -7,4 +7,15 @@ require_once __DIR__.'/../vendor/autoload.php'; //o autoload carrega todas as cl
 use App\Misu\Controllers\UsuarioController ;
 
 $usuarioController = new UsuarioController();
+$caminho = $_SERVER["REQUEST_URI"] ?? '/';
+$metodo =  $_SERVER["REQUEST_METHOD"] ?? 'GET';
+
+if($caminho === '/src/api' && $metodo === 'GET'){
+    echo "Bem-vindo API";
+
+}else if(strtolower ($caminho) === '/src/usuarios' && $metodo === 'GET'){
+    $usuarioController->listar();
+}
+
+
 $usuarioController-> converterEmArray();
