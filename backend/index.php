f5bbe283ac5ca2ab5b063bb7da0ff181ed4f9f4d
heve-dev <hevellin.jsantos@hotmail.com>
2025-10-07 08:51:23 -0300

    mudanças no index.php com bramus

--- backend/index.php
+++ backend/index.php
@@ -1,71 +1,26 @@
 <?php
-
 namespace App\Misu;
+require_once __DIR__.'/../vendor/autoload.php';
 
-require_once __DIR__.'/../vendor/autoload.php'; //o autoload carrega todas as classes do composer
-
-if (!isset($_SESSION)) {
-            session_start();
-        }
+use Bramus\Router\Router;
+$router = new Router();
+$router->setNamespace('App\Misu\Controllers');
 
 use App\Misu\Rotas\Rotas;
-
 $rotas = Rotas::get();
 
-// rotas estara mapeando a classe e o metodo
-$metodoHttp = $_SERVER["REQUEST_METHOD"];
-$rota = $_SERVER["REQUEST_URI"];
-if(array_key_exists($rota, $rotas[$metodoHttp]) == false ){
-    http_response_code(404);
-    echo "Página não foi encontrada :(";
-    exit;
+foreach ($rotas as $metodoHttp => $rota) {
+    foreach ($rota as $uri => $acao) {
+        $router->{$metodoBramus}($uri, $acao); // a mágica acontece aqui
+    }
 }
-//     retorno string para separar em partes
-$partes = explode("@", $rotas[$metodoHttp][$rota] );
-$nomeController = $partes[0];
-$metodoController = $partes[1];
-$nomeCompletoController = "App\\Misu\\Controllers\\". $nomeController;
-if(!class_exists($nomeCompletoController)){
-    http_response_code(500);
-    echo "O controlador não foi encontrado :(";
-    exit;
-}
-$controller = new $nomeCompletoController();
-$controller->$metodoController();
 
+$router->set404(function() {
+    header($_SERVER['SERVER_PROTOCOL'] . '404 Not Found');
+    echo '404, Rota não encontrada :( ';
+});
 
+$router->run();
 
-
-// var_dump ($partes);
-
-//23/09
-// var_dump($_SERVER["REQUEST_URI"]);
-// echo "\n\n\n\n";
-// var_dump($_REQUEST["REQUEST_METHOD"]);
-// exit;
-// if(($_SERVER["REQUEST_URI"]) =="/backend/buscarUsuario" && $_SERVER["REQUEST_METHOD"] == "GET")
-// {
-//     $controller = new UsuarioController();
-//     $resultado = $controller->index();
-//     var_dump($resultado);
-// }else{
-//     echo "rota não encontrada";
-// }
-
-// // 26/09
-
-// $usuarioController = new UsuarioController();
-// $caminho = $_SERVER["REQUEST_URI"] ?? '/';
-// $metodo =  $_SERVER["REQUEST_METHOD"] ?? 'GET';
-
-// if($caminho === '/src/api' && $metodo === 'GET'){
-//     echo "Bem-vindo API";
-
-// }else if(strtolower ($caminho) === '/src/usuarios' && $metodo === 'GET'){
-//     $usuarioController->listar();
-// }
-
-
-// $usuarioController-> converterEmArray();
 
 
