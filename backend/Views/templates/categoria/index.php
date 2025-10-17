a6a1db7ca35648f40ecd719ef8f464741e2d1268
heve-dev <hevellin.jsantos@hotmail.com>
2025-10-17 10:00:29 -0300

    exc de tabelas

--- backend/Views/templates/categoria/index.php
+++
@@ -1,8 +0,0 @@
-<div>Sou o index</div>
-<php foreach($categoria): ?>
-    <p><?= $categoria['id_categoria'] ?></p>
-    <p><?= $categoria['nome_categoria'] ?></p>
-    <p><?= $categoria['tipo_categoria'] ?></p>
-    <p><?= $categoria['status_categoria'] ?></p>
-
-    <php endforeach; ?>
